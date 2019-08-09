6e413d81895c11b69cd43387c1bd114af8e91482
CircleCI Deploy Job <ci-build@circleci.com>
2019-08-09 14:08:58 +0000

    Deploy webpage on 2019-08-09
    Builds https://github.com/hetio/disease-genes-frontend/commit/4e97f2a2d0ddda205d1a34611e8cf54cff26a5c6
    Pushed by https://circleci.com/gh/hetio/disease-genes-frontend/3
    
    [skip ci]

---
+++ precache-manifest.da2ef391b15522f54922a2a81bd160b6.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0c0228193ae9fdb29b296476710bae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b186e4267352624137bb",
+    "url": "./static/css/main.css"
+  },
+  {
+    "revision": "b186e4267352624137bb",
+    "url": "./static/js/main.js"
+  }
+]);
