b44a75f1179515398c39aef5c34d828c749e854d
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-08-12 15:38:18 -0400

    add external links

--- src/disease-info.js
+++ src/disease-info.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { toFixed } from 'hetio-frontend-components';
 import { InfoTable } from 'hetio-frontend-components';
@@ -16,8 +18,34 @@ export class DiseaseInfo extends Component {
     const name = disease.disease_name;
     const info = (this.props.diseaseInfo || {}).xref || {};
     const bodyContents = [
-      ['Dis. Ont. id', tooltipText['disease_id'], disease.disease_code],
-      ['EFO id', tooltipText['disease_efo_id'], info.EFO],
+      [
+        'Dis. Ont. id',
+        tooltipText['disease_id'],
+        <a href={'http://www.disease-ontology.org/?id=' + disease.disease_code}>
+          {disease.disease_code}
+          <FontAwesomeIcon
+            className='external_link_icon'
+            icon={faExternalLinkAlt}
+            size='xs'
+          />
+        </a>
+      ],
+      [
+        'EFO id',
+        tooltipText['disease_efo_id'],
+        <a
+          href={
+            'https://www.ebi.ac.uk/ols/ontologies/EFO/terms?obo_id=EFO:' + info.EFO
+          }
+        >
+          {info.EFO}
+          <FontAwesomeIcon
+            className='external_link_icon'
+            icon={faExternalLinkAlt}
+            size='xs'
+          />
+        </a>
+      ],
       ['UMLS id', tooltipText['disease_umls_id'], info.UMLS_CUI],
       [
         'pathophysiology',
--- src/gene-prediction-info.js
+++ src/gene-prediction-info.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { toFixed } from 'hetio-frontend-components';
 import { InfoTable } from 'hetio-frontend-components';
@@ -16,8 +18,39 @@ export class GenePredictionInfo extends Component {
     const name = genePrediction.disease_name;
     const info = (this.props.genePredictionInfo || {}).xref || {};
     const bodyContents = [
-      ['Dis. Ont. id', tooltipText['disease_id'], genePrediction.disease_code],
-      ['EFO id', tooltipText['disease_efo_id'], info.EFO],
+      [
+        'Dis. Ont. id',
+        tooltipText['disease_id'],
+        <a
+          href={
+            'http://www.disease-ontology.org/?id=' + genePrediction.disease_code
+          }
+        >
+          {genePrediction.disease_code}
+          <FontAwesomeIcon
+            className='external_link_icon'
+            icon={faExternalLinkAlt}
+            size='xs'
+          />
+        </a>
+      ],
+      [
+        'EFO id',
+        tooltipText['disease_efo_id'],
+        <a
+          href={
+            'https://www.ebi.ac.uk/ols/ontologies/EFO/terms?obo_id=EFO:' +
+            info.EFO
+          }
+        >
+          {info.EFO}
+          <FontAwesomeIcon
+            className='external_link_icon'
+            icon={faExternalLinkAlt}
+            size='xs'
+          />
+        </a>
+      ],
       ['UMLS id', tooltipText['disease_umls_id'], info.UMLS_CUI],
       [
         'pathophysiology',
