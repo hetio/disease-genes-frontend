12a2fb5201f718495e34480b8fb6e8c298f5e581
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-09-20 10:14:55 -0400

    update package depencies (#16)

---
+++ build/precache-manifest.e5a3a3cd04f340a2ab1681f8298be443.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff0853d90c9e5832118007d0b755b63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "827c7dae7e48bde58f60",
+    "url": "./static/css/main.68ffd499.css"
+  },
+  {
+    "revision": "827c7dae7e48bde58f60",
+    "url": "./static/js/main.js"
+  }
+]);
